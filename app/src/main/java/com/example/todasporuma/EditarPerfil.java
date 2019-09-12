329f4d11c7d319873b25820a4955b4b6eccc161f
Telas principais do App <mateus_lima98@hotmail.com>
2019-09-11 22:08:41 -0300

    Todas por uma

---
+++ app/src/main/java/com/example/todasporuma/EditarPerfil.java
@@ -0,0 +1,13 @@
+package com.example.todasporuma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+    }
+}
